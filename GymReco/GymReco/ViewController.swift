185f4b5cc50217356658fad2364f6bc0abc67152
tomo8104 <kimto8104@gmail.com>
2020-03-08 20:39:07 +0900

    First Commit. Project is created.

---
+++ GymReco/GymReco/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GymReco
+//
+//  Created by Tomofumi Kimura on 2020/03/08.
+//  Copyright © 2020 tomo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
