e100b49c0a7811733d0a814247b7781c9bb79191
tomo8104 <kimto8104@gmail.com>
2020-03-12 09:17:15 +0900

    Add Navigation Bar for Home and RecordPage

--- GymReco/GymReco/Home/HomeViewController.swift
+++ GymReco/GymReco/Home/HomeViewController.swift
@@ -9,22 +9,33 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    // ナビゲーションバーボタンアイテムの宣言
+    var listButton: UIBarButtonItem! // 記録画面へのボタン
+    var sideMenuButton: UIBarButtonItem! // 左サイドメニューボタン
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setUp()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // setup page
+    func setUp() {
+        // ナビゲーションボタンの初期化
+        listButton = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: self, action: #selector(tapListButton(_ :)))
+        // 左サイドメニューボタンの初期化
+        sideMenuButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(tapListButton(_ :)))
+        //ナビゲーションボタンの追加
+        self.navigationItem.rightBarButtonItem = listButton
+        self.navigationItem.leftBarButtonItem = sideMenuButton
+        
     }
-    */
+        
+    @objc func tapListButton(_ sender: UIBarButtonItem) {
+        // Record　ページに画面遷移させる
+        let recordPageVC = RecordPageViewController()
+        self.show(recordPageVC, sender: self)
+        
+    }
 
 }
--- GymReco/GymReco/RecordPage/RecordPageViewController.swift
+++ GymReco/GymReco/RecordPage/RecordPageViewController.swift
@@ -9,8 +9,23 @@
 import UIKit
 
 class RecordPageViewController: UIViewController {
+    
+    // ナビゲーションバー ボタンの宣言
+    var addRecordButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpRecordPage()
+    }
+    
+    // setup page
+    func setUpRecordPage() {
+        addRecordButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(tapAddRecordButton(_:)))
+        self.navigationItem.rightBarButtonItem = addRecordButton
+    }
+    
+    @objc func tapAddRecordButton(_ sender: UIBarButtonItem) {
+        // 記録追加画面に画面遷移する
+        
     }
 }
