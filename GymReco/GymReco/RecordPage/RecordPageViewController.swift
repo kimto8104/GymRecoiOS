4f2f5e421509049b9433a600763c6f25d7fba83e
tomo8104 <kimto8104@gmail.com>
2020-03-21 14:02:16 +0900

    記録編集ページ実装

--- GymReco/GymReco/RecordPage/RecordPageViewController.swift
+++ GymReco/GymReco/RecordPage/RecordPageViewController.swift
@@ -7,14 +7,18 @@
 //
 
 import UIKit
-
+// 記録ページ
 class RecordPageViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
     // ナビゲーションバー ボタンの宣言
     var addRecordButton: UIBarButtonItem!
-
+    private let monthSection: Array = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月","12月"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "DayRecordTableViewCell", bundle: nil), forCellReuseIdentifier: "DayRecordCell")
         setUpRecordPage()
     }
     
@@ -29,4 +33,40 @@ class RecordPageViewController: UIViewController {
         // 記録追加画面に画面遷移する
         
     }
+}
+
+extension RecordPageViewController: UITableViewDelegate {
+    
+}
+
+extension RecordPageViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let dayRecordCell = tableView.dequeueReusableCell(withIdentifier: "DayRecordCell", for: indexPath)
+        return dayRecordCell
+    }
+    
+    // セクション数
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return monthSection.count
+    }
+    // ヘッダーセクションの文言
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return monthSection[section] as? String
+    }
+    
+    // 各セルタップアクション
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let editRecordVC = EditRecordPageViewController()
+        self.show(editRecordVC, sender: self)
+    }
+    
+    
+    
+    
+    
+    
 }
