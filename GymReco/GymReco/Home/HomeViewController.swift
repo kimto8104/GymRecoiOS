8196cc80d94e041bf222a088ead54f4592cf0f5c
tomo8104 <kimto8104@gmail.com>
2020-04-25 01:47:11 +0900

    ホームタブ/記録/設定　下タブ追加

--- GymReco/GymReco/Home/HomeViewController.swift
+++ GymReco/GymReco/Home/HomeViewController.swift
@@ -9,39 +9,9 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-    /// IBOutlet
-    // ナビゲーションバーボタンアイテムの宣言
-    var listButton: UIBarButtonItem! // 記録画面へのボタン
-    var sideMenuButton: UIBarButtonItem! // 左サイドメニューボタン
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setUpNavigation()
-    }
-    
-    // setup page
-    func setUpNavigation() {
-        // ナビゲーションボタンの初期化
-        listButton = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: self, action: #selector(tapListButton(_ :)))
-        // 左サイドメニューボタンの初期化
-        sideMenuButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(openSideMenu(_ :)))
-        //ナビゲーションボタンの追加
-        self.navigationItem.rightBarButtonItem = listButton
-        self.navigationItem.leftBarButtonItem = sideMenuButton
-        // ナビゲーションタイトル追加
-        self.navigationItem.title = "Home"
-    }
-    
-    
-    
-    @objc func tapListButton(_ sender: UIBarButtonItem) {
-        // Record　ページに画面遷移させる
-        let recordPageVC = RecordPageViewController()
-        self.show(recordPageVC, sender: self)
-        
-    }
-    
-    @objc func openSideMenu(_ sender: UIBarButtonItem) {
-        
     }
     
 }
